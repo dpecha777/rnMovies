e13737cb7018bfe5e8a70d22418cf558854792ea
Dominik Pecha <dom.pecha@gmail.com>
2022-04-02 23:28:57 +0200

    android fix http requests, localhost url

--- src/services/api/movies.ts
+++ src/services/api/movies.ts
@@ -1,12 +1,18 @@
 import {stores} from '../../stores';
 
+import {Platform} from 'react-native';
+
+// url for the local api using json-server
+// android doesn't like localhost xD
+const url = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
+
 export class MoviesApi {
   getCarousels = async (): PVoid => {
     const {movies} = stores;
 
     movies.setLoading(true);
 
-    const res = await fetch('http://localhost:3000/carousels');
+    const res = await fetch(url + '/carousels');
     const json: CarouselsWithMovies = await res.json();
 
     movies.setCarousels(json);
@@ -18,7 +24,7 @@ export class MoviesApi {
 
     movies.setLoading(true);
 
-    const res = await fetch(`http://localhost:3000/movies/${id}`);
+    const res = await fetch(url + `/movies/${id}`);
     const json: Movie = await res.json();
 
     movies.setCurrentMovieDetail(json);
